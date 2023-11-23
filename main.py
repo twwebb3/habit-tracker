f643863c146f78136b53c93d3de6378f3519ca5d
TW Webb <twwebb1@me.com>
2023-11-23 16:33:37 -0500

    Menu bar added.

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QMenuBar, QAction
 
 class HabitTracker(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Set the title of the window
+        # Set the title and size of the window
         self.setWindowTitle("Habit Tracker")
-
-        # Set the size of the window
         self.setGeometry(300, 300, 400, 300)
 
-        # Add a label as an example
-        self.label = QLabel("Welcome to the Habit Tracker", self)
-        self.label.adjustSize()
-        self.label.move(10, 200)
+        # Setup UI elements
+        self.setupUI()
 
     def setupUI(self):
-        # This method can be used to set up UI elements
-        pass
+        # Create a menu bar
+        self.menuBar = self.menuBar()
+
+        # Create 'File' menu
+        fileMenu = self.menuBar.addMenu('File')
+
+        # Add 'New' action
+        newAction = QAction('New', self)
+        fileMenu.addAction(newAction)
+
+        # Add 'Open' action
+        openAction = QAction('Open', self)
+        fileMenu.addAction(openAction)
+
+        # Add 'Exit' action
+        exitAction = QAction('Exit', self)
+        exitAction.triggered.connect(self.close)  # Connect the action to close the window
+        fileMenu.addAction(exitAction)
+
+        # Add a label
+        self.label = QLabel("Welcome to the Habit Tracker", self)
+        self.label.adjustSize()
+        self.label.move(50, 130)
 
 def main():
     app = QApplication(sys.argv)
