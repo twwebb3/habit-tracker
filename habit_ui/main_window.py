37b38eed1e934f0e09d6343204759e9f0f3a33a3
TW Webb <twwebb1@me.com>
2023-12-07 14:15:27 -0500

    Habit form and list added to UI main window.

--- habit_ui/main_window.py
+++ habit_ui/main_window.py
@@ -1,68 +1,24 @@
 
 
-from PyQt5.QtWidgets import QMainWindow, QAction, QLabel, QListWidget, QCheckBox, QListWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout
+from .habit_form import HabitForm
+from .habit_list import HabitList
+
 class HabitTracker(QMainWindow):
     def __init__(self):
         super().__init__()
 
         # Set the title and size of the window
         self.setWindowTitle("Habit Tracker")
-        self.setGeometry(300, 300, 400, 300)
+        self.setGeometry(100, 100, 600, 400)
 
-        # Setup UI elements
-        self.setupUI()
+        # Central widget and layout
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
+        self.layout = QVBoxLayout(self.central_widget)
 
-
-    def setupUI(self):
-        # Create a menu bar
-        self.menuBar = self.menuBar()
-
-        # Create 'File' menu
-        fileMenu = self.menuBar.addMenu('File')
-
-        # Add 'New' action
-        newAction = QAction('New', self)
-        fileMenu.addAction(newAction)
-
-        # Add 'Open' action
-        openAction = QAction('Open', self)
-        fileMenu.addAction(openAction)
-
-        # Add 'Exit' action
-        exitAction = QAction('Exit', self)
-        exitAction.triggered.connect(self.close)  # Connect the action to close the window
-        fileMenu.addAction(exitAction)
-
-        # Add a label
-        self.label = QLabel("Welcome to the Habit Tracker", self)
-        self.label.adjustSize()
-        self.label.move(50, 130)
-
-        self.checklist = QListWidget(self)
-        self.checklist.setGeometry(50, 50, 400, 300)
-
-        # Add items to the checklist
-        self.addChecklistItems(["Habit 1", "Habit 2", "Habit 3"])
-
-
-    def addChecklistItems(self, items):
-        for item_text in items:
-            # Create a QListWidgetItem
-            item = QListWidgetItem(self.checklist)
-
-            # Create a QCheckBox for the item
-            chkBox = QCheckBox(item_text)
-
-            # Add the QCheckBox to the list
-            self.checklist.setItemWidget(item, chkBox)
-
-
-    def onCheckBoxStateChanged(self, state):
-        # This method is called when a checkbox is checked or unchecked
-        sender = self.sender()
-        if state == 2:  # Checked
-            print(f"{sender.text()} checked")
-            # Implement your logic for a checked habit here
-        else:  # Unchecked
-            print(f"{sender.text()} unchecked")
-            # Implement your logic for an unchecked habit here
+        # Add habit form and list to the layout
+        self.habit_list = HabitList(self)
+        self.habit_form = HabitForm(self)
+        self.layout.addWidget(self.habit_list)
+        self.layout.addWidget(self.habit_form)
