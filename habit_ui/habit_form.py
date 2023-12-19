d2a6eea9b79c2cb3f8ef37d62353690dfd1763be
TW Webb <twwebb1@me.com>
2023-12-18 21:47:20 -0500

    Habit frequency option added.

--- habit_ui/habit_form.py
+++ habit_ui/habit_form.py
@@ -1,5 +1,5 @@
 
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QComboBox
 
 class HabitForm(QWidget):
     def __init__(self, parent=None):
@@ -11,12 +11,22 @@ class HabitForm(QWidget):
         self.habit_input = QLineEdit(self)
         self.layout.addWidget(self.habit_input)
 
+        # Frequency selection
+        self.layout.addWidget(QLabel("Select a frequency:"))
+        self.frequency_combo = QComboBox(self)
+        self.frequency_combo.addItems(["Daily", "Weekly", "Monthly", "Quarterly"])
+        self.layout.addWidget(self.frequency_combo)
+
         self.add_button = QPushButton("Add Habit", self)
         self.layout.addWidget(self.add_button)
         self.add_button.clicked.connect(self.add_habit)
 
     def add_habit(self):
-        habit = self.habit_input.text()
+        habit_name = self.habit_input.text()
+        habit_frequency = self.frequency_combo.currentText()
         # Logic to add the habit to the list and storage
-        print("Habit to add:", habit)
+        print("Habit to add:", habit_name, "Frequency:", habit_frequency)
+        # Clear the input field
+        self.habit_input.clear()
+        self.frequency_combo.setCurrentIndex(0)
 
