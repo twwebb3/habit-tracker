383858e862c3d79fef70d101bbc03dcd49bd80e9
TW Webb <twwebb1@me.com>
2024-01-04 16:14:11 -0500

    Habit form retrieves and lists habits in db.

--- habit_ui/habit_list.py
+++ habit_ui/habit_list.py
@@ -2,11 +2,22 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QCheckBox
 
 class HabitList(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, database=None):
         super().__init__(parent)
+        self.database = database
         self.layout = QVBoxLayout(self)
 
-        # Example checkbox (you will dynamically add checkboxes based on stored habits)
-        self.example_checkbox = QCheckBox("Example Habit", self)
-        self.layout.addWidget(self.example_checkbox)
+        self.load_habits()
 
+    def load_habits(self):
+        # Retrieve all habits from the database
+        habits = self.database.get_all_habits()
+        for habit in habits:
+            # Create a checkbox for each habit
+            checkbox = QCheckBox(habit[1], self)
+            self.layout.addWidget(checkbox)
+            # Retrieve all completion records for the habit
+            completions = self.database.get_completions(habit[0])
+            for completion in completions:
+                # Add a checkmark to the checkbox for each completion
+                checkbox.setCheckState(2)
