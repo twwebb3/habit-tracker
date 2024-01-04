382a16de8d4ba8ab89cf7f5369165b4d15dc5aac
TW Webb <twwebb1@me.com>
2024-01-04 16:11:48 -0500

    Added database connection and initialization.

--- app.py
+++ app.py
@@ -2,11 +2,15 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from habit_ui.main_window import HabitTracker
+from database.habit_database import HabitDatabase
 
 
 def main():
     app = QApplication(sys.argv)
-    mainWin = HabitTracker()
+
+    database = HabitDatabase("habits.db")
+
+    mainWin = HabitTracker(database)
     mainWin.show()
     sys.exit(app.exec_())
 
